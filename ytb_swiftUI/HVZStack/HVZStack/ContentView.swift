f9b4f38c345a4799985b9eb77bf322aa422f297a
js41 <tyongzhan@gamil.com>
2019-11-08 20:02:38 +0700

    swfitUI  HVZStack

---
+++ ytb_swiftUI/HVZStack/HVZStack/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HVZStack
+//
+//  Created by pm on 2019/11/8.
+//  Copyright © 2019 pm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
